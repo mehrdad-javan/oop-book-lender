7b05d969914d77a5ebb1e45ec17e02eebdebe383
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-08-09 13:16:19 +0200

    add java doc

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -2,6 +2,10 @@ package se.lexicon.model;
 
 import java.util.UUID;
 
+
+/**
+ * Represents a book in the library.
+ */
 public class Book {
 
     private String title;
@@ -11,70 +15,141 @@ public class Book {
     private final String id;
     private Person borrower;
 
+    /**
+     * Default constructor for creating a new book instance.
+     * Generates a unique ID for the book and sets it as available.
+     */
     private Book() {
         this.id = UUID.randomUUID().toString();
         this.available = true;
     }
 
+    /**
+     * Creates a book instance with the given title and author.
+     * Initializes the book with a unique ID and sets it as available.
+     *
+     * @param title  The title of the book.
+     * @param author The author of the book.
+     */
     public Book(String title, String author) {
         this();
         setTitle(title);
         setAuthor(author);
     }
 
+    /**
+     * Creates a book instance with the given title, author, and borrower.
+     *
+     * @param title    The title of the book.
+     * @param author   The author of the book.
+     * @param borrower The person borrowing the book.
+     */
     public Book(String title, String author, Person borrower) {
         this(title, author);
         setBorrower(borrower);
     }
 
+    /**
+     * Gets the unique ID of the book.
+     *
+     * @return The unique ID of the book.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the title of the book.
+     *
+     * @return The title of the book.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the book.
+     *
+     * @param title The title to set for the book.
+     * @throws IllegalArgumentException If the provided title is null.
+     */
     public void setTitle(String title) {
         if (title == null) throw new IllegalArgumentException("Title cannot be null");
         this.title = title;
     }
 
+    /**
+     * Gets the author of the book.
+     *
+     * @return The author of the book.
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Sets the author of the book.
+     *
+     * @param author The author to set for the book.
+     * @throws IllegalArgumentException If the provided author is null.
+     */
     public void setAuthor(String author) {
         if (author == null) throw new IllegalArgumentException("Author cannot be null");
         this.author = author;
     }
 
+    /**
+     * Checks if the book is available for borrowing.
+     *
+     * @return true if the book is available, false otherwise.
+     */
     public boolean isAvailable() {
         return available;
     }
 
+    /**
+     * Gets the person who has borrowed the book.
+     *
+     * @return The person who has borrowed the book.
+     */
     public Person getBorrower() {
         return borrower;
     }
 
+    /**
+     * Sets the borrower for the book and updates availability accordingly.
+     *
+     * @param borrower The person who borrows the book.
+     */
     public void setBorrower(Person borrower) {
         this.borrower = borrower;
         this.available = (borrower == null);
     }
 
+    /**
+     * Retrieves information about the person who has borrowed the book.
+     *
+     * @return Information about the borrower of the book.
+     */
     public String showPerson() {
         return borrower.getPersonInformation();
     }
 
-  /*public Person showPerson() {
-    return borrower;
-  }*/
+     /*public Person showPerson() {
+        return borrower;
+      }*/
 
+    /**
+     * Generates a string representation of the book's information.
+     *
+     * @return A formatted string containing book details.
+     */
     public String getBookInformation() {
         // Java Ternary Operator
         // https://careerkarma.com/blog/java-ternary-operator/
         // variable = (expression) ? expressionIsTrue : expressionIsFalse;
-        return "Book{ id=" + id + ", title=" + title + ", author=" + author + ", available=" + available + ", borrower=" + (borrower != null ? " PersonId: " + borrower.getPersonId() : "-") + " } ";
+        return "Book{ id=" + id + ", title=" + title + ", author=" + author + ", available=" + available +
+                ", borrower=" + (borrower != null ? " PersonId: " + borrower.getPersonId() : "-") + " } ";
     }
 
 }
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -1,7 +1,11 @@
 package se.lexicon.model;
 
+
 import java.util.Arrays;
 
+/**
+ * Represents a person in the library system who can borrow books.
+ */
 public class Person {
 
     private final Integer personId;
@@ -10,17 +14,34 @@ public class Person {
 
     private Book[] borrowedBooks;
 
+    /**
+     * Default constructor for creating a new person instance.
+     * Generates a unique person ID and initializes an empty array of borrowed books.
+     */
     private Person() {
         this.personId = PersonSequencer.getNextId();
         this.borrowedBooks = new Book[0];
     }
 
+    /**
+     * Creates a person instance with the given first name and last name.
+     * Initializes a unique person ID and an empty array of borrowed books.
+     *
+     * @param firstName The first name of the person.
+     * @param lastName  The last name of the person.
+     */
     public Person(String firstName, String lastName) {
         this();
         setFirstName(firstName);
         setLastName(lastName);
     }
 
+    /**
+     * Borrows a book and adds it to the list of borrowed books.
+     *
+     * @param book The book to be borrowed.
+     * @throws IllegalArgumentException If the provided book is null or not available.
+     */
     public void loanBook(Book book) {
         if (book == null) throw new IllegalArgumentException("Book cannot be null");
 
@@ -34,6 +55,12 @@ public class Person {
         }
     }
 
+    /**
+     * Returns a borrowed book and removes it from the list of borrowed books.
+     *
+     * @param book The book to be returned.
+     * @throws IllegalArgumentException If the provided book is null.
+     */
     public void returnBook(Book book) {
         if (book == null) throw new IllegalArgumentException("Book cannot be null");
 
@@ -49,10 +76,21 @@ public class Person {
         borrowedBooks = newArray;
     }
 
+    /**
+     * Retrieves information about the person, including their ID, first name, last name, and number of borrowed books.
+     *
+     * @return Information about the person.
+     */
     public String getPersonInformation() {
-        return "Person{ personId=" + personId + ", firstName='" + firstName + ", lastName='" + lastName + ",number of borrowedBooks=" + borrowedBooks.length + '}';
+        return "Person{ personId=" + personId + ", firstName='" + firstName + ", lastName='" + lastName +
+                ",number of borrowedBooks=" + borrowedBooks.length + '}';
     }
 
+    /**
+     * Generates a string containing the details of books borrowed by the person.
+     *
+     * @return A formatted string with book information.
+     */
     public String displayBooks() {
         StringBuilder stringBuilder = new StringBuilder();
         if (borrowedBooks != null) {
@@ -63,29 +101,60 @@ public class Person {
         return stringBuilder.toString();
     }
 
-
+    /**
+     * Gets the unique ID of the person.
+     *
+     * @return The unique ID of the person.
+     */
     public Integer getPersonId() {
         return personId;
     }
 
+    /**
+     * Gets the first name of the person.
+     *
+     * @return The first name of the person.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the first name of the person.
+     *
+     * @param firstName The first name to set for the person.
+     * @throws IllegalArgumentException If the provided first name is null.
+     */
     public void setFirstName(String firstName) {
         if (firstName == null) throw new IllegalArgumentException("FirstName cannot be null");
         this.firstName = firstName;
     }
 
+    /**
+     * Gets the last name of the person.
+     *
+     * @return The last name of the person.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the last name of the person.
+     *
+     * @param lastName The last name to set for the person.
+     * @throws IllegalArgumentException If the provided last name is null.
+     */
     public void setLastName(String lastName) {
         if (lastName == null) throw new IllegalArgumentException("LastName cannot be null");
         this.lastName = lastName;
     }
 
+    /**
+     * Gets the array of borrowed books by the person.
+     *
+     * @return The array of borrowed books.
+     */
     public Book[] getBorrowedBooks() {
         return borrowedBooks;
     }
